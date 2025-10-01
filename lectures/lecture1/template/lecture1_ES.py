49cb9133d3014e71929c19a8674b0f91dba341d4
Einar Storvestre <einargaard@icloud.com>
2025-10-01 08:47:13 +0200

    hi

--- lectures/lecture1/template/lecture1_ES.py
+++ lectures/lecture1/template/lecture1_ES.py
@@ -24,6 +24,9 @@ def argmax(lst):
 
     N = len(lst)
 
+    if N == 0:
+        raise ValueError("The input list is empty.")
+    
     value_max = - np.inf
 
     for i in range(N):
@@ -34,7 +37,14 @@ def argmax(lst):
 
     return imax
 
-values = [2, 3, -1, 7, 4]
 
-i = argmax(values)
-print(f"The maximum value is at index: {i}")
+
+if __name__ == "__main__": #only execute this when you run this python file, and not if its used in other files
+    #values = [2, 3, -1, 7, 4]
+    values = []
+    try: 
+        i = argmax(values)
+        print(f"The maximum value is at index: {i}")
+    except ValueError:
+        print("Caught an exception: The input list is empty.")
+
